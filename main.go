96e6f9e711937aca9574184d2489ce1b745d6e9b
K0ntr4 <lpaulekat@yahoo.de>
2024-04-01 13:17:12 +0200

    added import for team functions to main file

--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
+import (
+	"github.com/K0ntr4/pokemon_battle_advisor/team"
+)
+
 func main() {
-	team := GetRandomTeam(0, 493)
+	team := team.GetRandomTeam(0, 493)
 	for _, pokemon := range team {
 		println(pokemon.Name)
 	}
